bb707b62c4b2cf041c9c6a0f5d7ee9cab9b223a1
Xess1407 <evanclausse@yahoo.fr>
2023-05-22 15:58:48 +0200

    insert resource

--- insert.sh
+++ insert.sh
@@ -12,9 +12,13 @@ curl -X POST -H "Content-Type: application/json" -d '{"name":"man","family_name"
 curl -X POST -H "Content-Type: application/json" -d '{"user_id":3, "company":"renault", "activation_date":"2023-05-17", "deactivation_date":"2023-05-18", "password": "man"}' localhost:8080/api/manager
 
 # Insert Challenge
-
 curl -X POST -H "Content-Type: application/json" -d '{"name": "MegaChallengeDeLaMortQuiTue", "date_time_start":"2023-05-17", "date_time_end":"2023-06-12", "password": "admin"}' localhost:8080/api/challenge
 
 # Insert Project
-
 curl -X POST -H "Content-Type: application/json" -d '{"data_challenge_id": 1, "name": "Lyoko", "description": "Un projet autour des données de XANA", "image": "identifiantDuneImage", "password": "admin"}' localhost:8080/api/project
+
+# Insert ResourceChallenge
+curl -X POST -H "Content-Type: application/json" -d '{"data_challenge_id":1, "name":"Image", "url":"./blabla", "password": "admin"}' localhost:8080/api/resource-challenge
+
+# Insert ResourceChallenge
+curl -X POST -H "Content-Type: application/json" -d '{"data_project_id":1, "name":"Image", "url":"./blabla", "password": "admin"}' localhost:8080/api/resource-project
