f3b6d7851905afd5e58afcbcd8ffb79b7e917884
leeo-p <portetleo@gmail.com>
2023-05-23 11:15:24 +0200

    link button home register to page register

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home: Component = () => {
             <Flex direction="row">
                 <Flex direction="column" mt="2%" ml="15%">
                     <ButtonCustom text="SIGN-IN" ff="Roboto black" fsz="16px" w="183px" h="48" br="16px" bgc="#3BCFA3" mb="10%"></ButtonCustom>
-                    <ButtonCustom text="SIGN-UP" ff="Roboto black" fsz="16px" w="183px" h="48" br="16px" bgc="#3BCFA3"></ButtonCustom>
+                    <a href="/register"><ButtonCustom text="SIGN-UP" ff="Roboto black" fsz="16px" w="183px" h="48" br="16px" bgc="#3BCFA3"></ButtonCustom></a>
                 </Flex>
                 <Box c="#FFFFFF" w="35%" ml="30%" mt="2%" fsz="17px" ff="Roboto">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean et. 
