a82d50a1c8b91f09fced63c69ceb5b472c1bfd4f
leeo-p <portetleo@gmail.com>
2023-05-24 16:51:45 +0200

    update navbar sub-menu

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,30 +1,48 @@
-import { Component, createSignal, Show } from "solid-js";
+import { Component, createSignal, Show} from "solid-js";
 import Flex from "./layouts/Flex";
 import Box from "./layouts/Box";
 import linkItems from "./linkItems";
-import "./css/Header.css"
-
+import "./css/Header.css";
 
 const Header: Component = () => {
-    const [boxData, setBoxData] = createSignal(false)
+    const [boxData, setBoxData] = createSignal(false);
+
+    const handle = () => {
+        setBoxData(!boxData());
+        const img = document.getElementById("down")
+        if (img) {
+            if (boxData()) {
+                img.style.transform = "rotate(180deg)"
+            } else {
+                img.style.transform = "rotate(0deg)"
+            }
+        }
+    };
+
     return (
         <Box b="1px solid" bgc="rgba(0, 0, 0, 0.7)" opt="" h="140px">
-            <Flex jc="center" h="100%" ai="center" td="none" ff="Roboto">
-                    <img src="src/img/logo.png" alt="logo" height="100px"/>
-                    <ul>{linkItems("Accueil" ,"")}</ul>
-                    <ul>{linkItems("Profil", "connect")}</ul>
-                    <ul>{linkItems("Equipe", "register")}</ul>
-                    <Flex ff="Roboto" direction="column" >
-                        <ul class="data-challenge">{linkItems("Data Challenges", "")}</ul>
-                        <Show when={boxData()}>
-                            <ul class="data-project">{linkItems("Data Projects", "")}</ul>
-                        </Show>
-                    </Flex>
-                    <ul><img class="down" src="src/img/down.png" alt="down" onClick={() => {setBoxData(!boxData())}}/></ul>
-                    <ul>{linkItems("Messagerie", "contact")}</ul>
+        <Flex jc="center" h="100%" ai="center" td="none" ff="Roboto">
+            <img src="src/img/logo.png" alt="logo" height="100px" />
+            <ul>{linkItems("Accueil", "")}</ul>
+            <ul>{linkItems("Profil", "connect")}</ul>
+            <ul>{linkItems("Equipe", "register")}</ul>
+            <Flex ff="Roboto" direction="column">
+                <ul classList={{ "data-challenge": true, active: boxData() }}>
+                    {linkItems("Data Challenges", "")}
+                </ul>
+                <Show when={boxData()}>
+                    <ul classList={{ "data-project": true, active: boxData()}} style={{ display: "block", position: "absolute", top: "8%" }} >
+                        {linkItems("Data Projects", "")}
+                    </ul>
+                </Show>
             </Flex>
+            <ul>
+                <img class="down" id="down" src="src/img/down.png" alt="down" onClick={handle}/>
+            </ul>
+            <ul>{linkItems("Messagerie", "contact")}</ul>
+        </Flex>
         </Box>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
