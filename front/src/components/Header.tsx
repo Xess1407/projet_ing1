be1aa62c50b3177f7a27e73402dab5ea9bc5d245
Xess1407 <evanclausse@yahoo.fr>
2023-05-26 13:13:33 +0200

    fix LinkItems

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -6,6 +6,7 @@ import "./css/Header.css";
 import { isConnected } from "./Session";
 import { deconnection } from "./forms/ConnectForm";
 import { useNavigate } from "@solidjs/router";
+import LinkItems from "./linkItems";
 
 export const [connected, setConnected] = createSignal(isConnected())
 
@@ -30,23 +31,21 @@ const Header: Component = () => {
         <Box bgc="#000000" h="140px" m="0" p="0">
             <Flex jc="center" w="100%" m="0" p="0" h="100%" ai="center" td="none" ff="Roboto">
                 <img src="src/img/logo.png" alt="logo" height="100px" />
-                <ul>{linkItems("Home", "")}</ul>
-                <ul>{linkItems("Profile", "profile")}</ul>
-                <ul>{linkItems("Team", "team")}</ul>
+                <ul><LinkItems path="/" text={"Home"} /></ul>
+                <ul><LinkItems path="/profile" text={"Profile"} /></ul>
+                <ul><LinkItems path="/team" text={"Team"} /></ul>
                 <Flex ff="Roboto" direction="column">
-                    <ul classList={{ "data-challenge": true, active: boxData() }}>
-                        {linkItems("Data Challenges", "")}
-                    </ul>
+                    <ul classList={{ "data-challenge": true, active: boxData() }}><LinkItems path="/" text={"Data Challenges"} /></ul>
                     <Show when={boxData()}>
                         <ul classList={{ "data-project": true, active: boxData()}} style={{ display: "block", position: "absolute", top: "8%" }} >
-                            {linkItems("Data Projects", "")}
+                            <LinkItems path="/team" text={"Data project"} />
                         </ul>
                     </Show>
                 </Flex>
                 <ul>
                     <img class="down" id="down" src="src/img/down.png" alt="down" onClick={handle}/>
                 </ul>
-                <ul>{linkItems("Messaging", "")}</ul>
+                <ul><LinkItems path="/chat" text={"Chat"} /></ul>
                 <ul><Show when={connected()}><button onclick={() => {deconnection(); setConnected(false); nav("/redirect", { replace: true }); }}>Deconnexion</button></Show></ul>
             </Flex>
         </Box>
--- front/src/components/linkItems.tsx
+++ front/src/components/linkItems.tsx
@@ -1,5 +1,8 @@
 import {Link} from "@kobalte/core"
+import { useNavigate } from "@solidjs/router"
+import { Component } from "solid-js"
 
+/*
 function linkItems(s: string, link: string){
     return(
         <Link.Root class="link" href={"http://localhost:3000/" + link}>
@@ -7,5 +10,18 @@ function linkItems(s: string, link: string){
         </Link.Root>
     )
 }
+*/
 
-export default linkItems
+const LinkItems = (props: any) => {
+    const nav = useNavigate()
+    const handle_nav = () => {
+        nav(props.path, {replace: true})
+    }
+    return(
+        <Link.Root class="link" onclick={handle_nav}>
+            {props.text}
+        </Link.Root>
+    )
+}
+
+export default LinkItems
