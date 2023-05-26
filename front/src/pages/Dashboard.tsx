a17c3cb16d5e8cbf873a426e6546fda708b0630c
leeo-p <portetleo@gmail.com>
2023-05-26 11:06:04 +0200

    update dashboard

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -7,25 +7,24 @@ import ButtonCustom from "../components/generals/ButtonCustom";
 const Dashboard: Component = () => {
     return (
         <Flex bgc="#000000" w="100%" h="calc(100vh - 140px)" m="0" p="0" ovy="hidden" direction="row" jc="space-evenly">
-            <Flex bgc="#555555" br="10px" w="18%" h="50%">
-                <ul>
-                    <Flex>
-                        <img class="icons" src="src/img/analytics.png" alt="icon" />
-                        <li>Analytics</li>
-                    </Flex>      
-                    <Flex>
-                        <img class="icons" src="src/img/user.png" alt="icon" />
-                        <li>Users</li>
-                    </Flex>           
-                    <Flex>
-                        <img class="icons" src="src/img/team.png" alt="icon" />
-                        <li>Teams</li>
-                    </Flex>                
-                    <Flex>
-                        <img class="icons" src="src/img/challenge.png" alt="icon" />
-                        <li>Challenges</li>
-                    </Flex>
-                </ul>
+            <Flex direction="column" bgc="#555555" br="10px" w="18%" h="60%" jc="space-evenly" ai="center" c="#FFFFFF" ff="Roboto">
+                <h2>Dashboard</h2>
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                    <img class="icons" src="src/img/analytics.png" alt="icon" />
+                    <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Analytics"/>
+                </Flex>
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                    <img class="icons" src="src/img/user.png" alt="icon" />
+                    <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Users"/>
+                </Flex>
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                    <img class="icons" src="src/img/team.png" alt="icon" />
+                    <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Teams"/>
+                </Flex>
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                    <img class="icons" src="src/img/challenge.png" alt="icon" />
+                    <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Data Challenges"/>
+                </Flex>
             </Flex>
             <Flex bgc="#555555" br="10px" w="75%" h="90%">
             </Flex>
