0098887d17c690e0c5a2231f96bddb249888b432
leeo-p <portetleo@gmail.com>
2023-05-26 14:39:20 +0200

    update dashboard

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -13,7 +13,7 @@ const Dashboard: Component = () => {
                     <img class="icons" src="src/img/analytics.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Analytics"/>
                 </Flex>
-                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center" bgc="#666666" br="10px">
                     <img class="icons" src="src/img/user.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Users"/>
                 </Flex>
@@ -26,7 +26,34 @@ const Dashboard: Component = () => {
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Data Challenges"/>
                 </Flex>
             </Flex>
-            <Flex bgc="#555555" br="10px" w="75%" h="90%">
+            <Flex bgc="#444444" br="10px" w="30%" h="80%" direction="column">
+                <Flex w="100%" h="50%" direction="">
+                    <Flex directon="column" w="60%" jc="space-evenly" ai="center">
+                        <Box w="60%" h="60%" bgc="#222222" br="10px" c="#FFFFFF" ta="center">
+                            <h3>Total students</h3>
+                            <p>6527</p>
+                        </Box>
+                    </Flex>
+                    <Flex w="40%" direction="column" jc="space-evenly" ai="center">
+                        <ButtonCustom w="80%" ff="Roboto" bgc="#444444" b="2px solid #00FFF5" c="#00FFF5" text="Add Student"/>
+                        <ButtonCustom w="80%" ff="Roboto" bgc="#444444" b="2px solid #E36464" c="#E36464" text="Remove Student"/>
+                    </Flex>
+                </Flex>
+                <Flex w="100%" h="50%">
+                    <Flex directon="column" w="60%" jc="space-evenly" ai="center">
+                        <Box w="60%" h="60%" bgc="#222222" br="10px" c="#FFFFFF" ta="center">
+                            <h3>Total Managers</h3>
+                            <p>6527</p>
+                        </Box>
+                    </Flex>
+                    <Flex w="40%" direction="column" jc="space-evenly" ai="center">
+                        <ButtonCustom w="80%" ff="Roboto" bgc="#444444" b="2px solid #00FFF5" c="#00FFF5" text="Add Manager"/>
+                        <ButtonCustom w="80%" ff="Roboto" bgc="#444444" b="2px solid #E36464" c="#E36464" text="Remove Manager"/>
+                    </Flex>
+                </Flex>
+            </Flex>
+            <Flex bgc="#444444" br="10px" w="40%" h="80%">
+
             </Flex>
         </Flex>
     )
