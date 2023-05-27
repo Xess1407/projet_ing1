f934775c81b6fc0fe8eebf9f9973c92c2c21405a
Xess1407 <evanclausse@yahoo.fr>
2023-05-27 22:00:37 +0200

    basic auth system

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,6 +14,7 @@ import YourTeam from './pages/YourTeam';
 import Dashboard from './pages/Dashboard';
 import Redirect from './pages/Redirect';
 import DataChallenges from "./pages/DataChallenges";
+import { Auth, Guard } from './pages/Auth';
 
 const App: Component = () => {
   return (
@@ -23,11 +24,12 @@ const App: Component = () => {
         <Route path="/" element={<Home/>}/>
         <Route path="/connect" element={<Connect/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/team" element={<Team/>}/>
-        <Route path="/yourteam/:team_id" element={<YourTeam/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/profile" element={<div><Guard/><Profile/></div>}/>
+        <Route path="/team" element={<div><Guard/><Team/></div>}/>
+        <Route path="/yourteam/:team_id" element={<div><Guard/><YourTeam/></div>}/>
+        <Route path="/dashboard" element={<div><Guard/><Dashboard/></div>}/>
         <Route path="/redirect" element={<Redirect/>} />
+        <Route path="/guard-auth" element={<Auth/>} />
         <Route path="/datachallenges" element={<DataChallenges/>} />
         <Route path="*" element={<div><p>404 Error</p></div>}/>
       </Routes>
---
+++ front/src/pages/Auth.tsx
@@ -0,0 +1,23 @@
+import { Component } from "solid-js"
+import Flex from "../components/layouts/Flex"
+import ButtonCustom from "../components/generals/ButtonCustom"
+import { useNavigate } from "@solidjs/router"
+import { isConnected } from "../components/Session"
+
+export const Guard = () => {
+    const nav = useNavigate()
+    if (!isConnected()) 
+        nav("/guard-auth")
+    
+    return <Flex>Checking login...</Flex>
+}
+
+export const Auth: Component = () => {
+    const nav = useNavigate()
+    return (
+    <Flex jc="center">
+        <p>You must be login to access this page</p>
+        <button onclick={() => {nav("/", {replace:true})}}>Get back home</button>
+    </Flex>
+    )
+}
