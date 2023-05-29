176466c9483e54accbb171a0505a3bc5160e8f9f
leeo-p <portetleo@gmail.com>
2023-05-29 11:33:14 +0200

    dashboard menu

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -11,7 +11,7 @@ const Dashboard: Component = () => {
         <Flex bgc="#000000" w="100%" h="calc(100vh - 140px)" m="0" p="0" ovy="hidden" direction="row" jc="space-evenly">
             <Flex direction="column" bgc="#555555" br="10px" w="18%" h="60%" jc="space-evenly" ai="center" c="#FFFFFF" ff="Roboto">
                 <h2>Dashboard</h2>
-                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center" br="10px">
                     <img class="icons" src="../src/img/analytics.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Analytics"/>
                 </Flex>
@@ -19,11 +19,11 @@ const Dashboard: Component = () => {
                     <img class="icons" src="../src/img/user.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Users" onclick={() => nav("/dashboard/", {replace:true})}/>
                 </Flex>
-                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center" br="10px">
                     <img class="icons" src="../src/img/team.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Teams" onclick={() => nav("/dashboard/rank", {replace:true})}/>
                 </Flex>
-                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center">
+                <Flex direction="row" w="100%" h="20%" jc="space-evenly" ai="center" br="10px">
                     <img class="icons" src="../src/img/challenge.png" alt="icon" />
                     <ButtonCustom w="60%" h="50%" br="10px" fs="1.5em" ff="Roboto" fc="#ffffff" fw="bold" text="Data Challenges" onclick={() => nav("/dashboard/challenge", {replace:true})}/>
                 </Flex>
