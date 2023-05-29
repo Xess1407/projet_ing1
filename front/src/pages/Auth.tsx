c667448c46625ec55527f3c81ff1aa3d64a942e5
yan <lefastdriller@gmail.com>
2023-05-29 12:00:12 +0200

    guard-auth-admin + css guard-auth + body bgc to black in Home.css

--- front/src/pages/Auth.tsx
+++ front/src/pages/Auth.tsx
@@ -24,9 +24,11 @@ export const Auth: Component = () => {
     return (
     <Box w="100%" h="calc(100vh - 140px)" m="0" p="0" ovy="hidden">
         <img src="src/img/fond.jpg" alt="background" class="background"/>
-        <h1 class="text">
+        <Flex jc="center" ai="center">
+        <h1 class="text" style="margin: 8% 0 5% 0">
             <span>You must be login to access this page</span>
         </h1>
+        </Flex>
         <Flex direction="row" jc="center" ai="center">
             <Flex direction="column">
                 <Show when={!isConnected()}>
@@ -43,9 +45,20 @@ export const Auth: Component = () => {
 export const AdminAuth: Component = () => {
     const nav = useNavigate()
     return (
-    <Flex jc="center">
-        <p>You must be an administrator to access this page</p>
-        <button onclick={() => {nav("/", {replace:true})}}>Get back home</button>
-    </Flex>
+    <Box w="100%" h="calc(100vh - 140px)" m="0" p="0" ovy="hidden">
+        <img src="src/img/fond.jpg" alt="background" class="background"/>
+        <Flex jc="center" ai="center">
+            <h1 class="text" style="margin: 8% 0 5% 0;font-size: 85px" >
+                <span>You must be an administrator to access this page</span>
+            </h1>
+        </Flex>
+        <Flex direction="row" jc="center" ai="center">
+            <Flex direction="column">
+                <Show when={!isConnected()}>
+                    <ButtonCustom text="GET BACK HOME" ff="Roboto black" fsz="16px" w="183px" h="48px" br="16px" bgc="#3BCFA3" mb="10%" onclick={() => {nav("/", {replace:true})}}/>
+                </Show>
+            </Flex>
+        </Flex>
+    </Box>
     )
 }
