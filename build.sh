e50bee6d1b3b407e5b7bbc2c89c417bd742a05c9
Xess1407 <evanclausse@yahoo.fr>
2023-06-01 09:20:25 +0200

    build run script

---
+++ build.sh
@@ -0,0 +1,14 @@
+cd front/
+npm install
+npm run build 
+cd ..
+
+cd api/
+chmod 700 setup.sh
+npm install
+npm run build 
+cd ..
+
+cd java/
+chmod 700 run.sh
+cd ..
---
+++ run.sh
@@ -0,0 +1,18 @@
+killall node
+cd api/ 
+./setup.sh 
+npm start &
+process_id=$!
+cd ..
+
+sleep 2
+./ins_production.sh 
+
+cd java/
+./run.sh &
+cd ..
+
+cd front/
+npm start &
+
+wait $process_id
